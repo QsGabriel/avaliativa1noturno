89af37d585da9ef8bca31eb812ca334936dafc20
Gabriel Queiroz <142359384+QsGabriel@users.noreply.github.com>
2023-09-28 22:05:57 -0300

    rename

---
+++ quest3aval.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    char senha[5];
+    int caractere, senha2, checar2;
+    char checar[5];
+
+    scanf("%s", senha);
+    senha[4] = '\0';
+    
+    do{ 
+    scanf("%s", checar);
+    checar[4] = '\0';
+
+    senha2 = atoi(senha);
+    checar2 = atoi(checar);
+
+    if(senha2 == checar2){
+        printf("senha valida!\n");
+    } else{
+        printf("senha invalida!\n");
+    }
+    } while(senha2 != checar2);
+       
+    return 0;
+}
